c9b439a610084b8775cbd632cd3a40e6a4bfbd71
DEEPAK KUMAR MISHRA <d10278074@gmail.com>
2026-06-23 12:42:38 +0530

    Update footer text in footer_dashboard function

--- src/components/footer.py
+++ src/components/footer.py
@@ -18,7 +18,7 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created by deepak</p>  
+        <p style="font-weight:bold; color:black;"> Created by deepak and much love to</p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
