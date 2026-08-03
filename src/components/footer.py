208f59290ec27b83c6201083d2da98a990793694
DEEPAK KUMAR MISHRA <d10278074@gmail.com>
2026-08-03 16:47:50 +0530

    Update footer functions to use new logo URL

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    logo_url = "Black and White Simple Minimalist Modern Line Beauty Salon Logo"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
@@ -14,7 +14,7 @@ def footer_home():
 
 
 def footer_dashboard():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    logo_url = "Black and White Simple Minimalist Modern Line Beauty Salon Logo"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
