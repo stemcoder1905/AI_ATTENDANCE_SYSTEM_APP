1639a8c7f8a6aa56acddc3fe0db11abe5798a3d0
DEEPAK KUMAR MISHRA <d10278074@gmail.com>
2026-08-11 11:58:41 +0530

    Reformat footer functions for consistency

--- src/components/footer.py
+++ src/components/footer.py
@@ -19,7 +19,7 @@ def footer_dashboard():
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style="font-weight:bold; color:black;"> Created by deepak</p>  
-        <img src='{logo_url}' style='max-height:25px' />
+       
         </div>
                 
                 """, unsafe_allow_html=True)
