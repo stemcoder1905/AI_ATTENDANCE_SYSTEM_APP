b1b52f413f6384dbe9f659beb26c7c31eb00cd93
DEEPAK KUMAR MISHRA <d10278074@gmail.com>
2026-08-11 11:55:17 +0530

    Reformat footer functions in footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -7,7 +7,7 @@ def footer_home():
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style="font-weight:bold; color:white;"> AIMLnext </p>  
-        <img src='{logo_url}' style='max-height:25px' />
+        
         </div>
                 
                 """, unsafe_allow_html=True)
