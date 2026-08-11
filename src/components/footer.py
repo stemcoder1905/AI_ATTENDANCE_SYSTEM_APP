a814219c13c306b640216c13334f8be2fdf8f3a3
DEEPAK KUMAR MISHRA <d10278074@gmail.com>
2026-08-11 11:50:46 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -6,7 +6,7 @@ def footer_home():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
+        <p style="font-weight:bold; color:white;"> AIMLnext </p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
